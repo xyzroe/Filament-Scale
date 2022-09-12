8127dd4c471de3c62f032c1e163e590e6778507c
xyzroe <me@kogan.in.ua>
2022-09-12 23:59:55 +0300

    v0.1.0
    Test new build and version system

--- SourceFiles/Tools/push.sh
+++ SourceFiles/Tools/push.sh
@@ -40,7 +40,7 @@ function spaser {
 
 function commit {
     spaser
-    echo -e "v$ver\n\n$(cat commit)" > commit_temp
+    echo -e "v$ver\n\n$(cat ../../commit)" > commit_temp
     cat commit_temp
     spaser
     git commit -a --file=commit_temp
